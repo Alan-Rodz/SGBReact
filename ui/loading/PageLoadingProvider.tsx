abc3f572e5b805e7c4812918931281f69294f266
Alan Rodz <s061199@hotmail.com>
2022-12-22 00:51:27 -0600

    consistent

--- ui/loading/PageLoadingProvider.tsx
+++ ui/loading/PageLoadingProvider.tsx
@@ -25,7 +25,6 @@ export const PageLoadingProvider: React.FC<Props> = ({ children }) => {
 
   }, [Router.events]);
 
-
   // -- UI --------------------------------------------------------------------....
   return (
     <PageLoadingContext.Provider value={{ isPageLoading }}>
